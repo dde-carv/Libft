7be31fd7d5a297b197489b5f88080116ce336f86
Duarte Eusebio <167210903+dde-carv@users.noreply.github.com>
2024-05-11 12:08:20 +0100

    Update ft_lstlast.c

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -27,28 +27,3 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (last);
 }
-
-/* #include <stdio.h>
-int	main(void)
-{
-	t_list	*head;
-	t_list	*node1;
-	t_list	*node2;
-	t_list	*node3;
-	t_list	*node4;
-	t_list	*last;
-
-	head = NULL;
-	node1 = ft_lstnew("Node 1");
-	node2 = ft_lstnew("Node 2");
-	node3 = ft_lstnew("Node 3");
-	node4 = ft_lstnew("Node 4");
-	last = ft_lstnew("Last node");
-	ft_lstadd_back(&head, node1);
-	ft_lstadd_back(&head, node2);
-	ft_lstadd_back(&head, node3);
-	ft_lstadd_back(&head, node4);
-	ft_lstadd_back(&head, last);
-	printf("Last node is: %s\n", (char *)ft_lstlast(head)->content);
-	return (0);
-} */
